462c5f4a23d0865f8c8bb55ca4fd19f607334cf9
halime <halimeucarturk@gmail.com>
2026-04-30 10:38:44 +0200

    Header.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 "use client";
 
-import Header from "@/components/Header";
-
 export default function Home() {
   return (
     <>
-      <Header />
       <div>
         <h1>HEM</h1>
       </div>
