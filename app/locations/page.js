10949abab01ae76ca819647b1efe991b275c53a5
aLexicon99 <267594691+aLexicon99@users.noreply.github.com>
2026-05-03 18:48:44 +0200

    Show-Campground-Cards
    - Using Campground Cards
    - Added Loading component
    - Link to each location/campground

--- app/locations/page.js
+++ app/locations/page.js
@@ -1,14 +1,19 @@
 "use client";
 
+import Link from "next/link";
+export const dynamic = "force-dynamic";
 import Header from "@/components/Header";
+import Loading from "@/components/Loading";
 import { useEffect, useState } from "react";
+// import locations from "@/public/locations_data.json";
+import CampgroundCard from "@/components/CampgroundCard";
 
 export default function Locations() {
   const [Locations, setLocations] = useState([]);
 
   useEffect(() => {
     async function fetchLocations() {
-      const response = await fetch("/api/locations", { cache : 'no-cache' });
+      const response = await fetch("/api/locations", { cache: "no-cache" });
       const data = await response.json();
       setLocations(data);
     }
@@ -17,24 +22,29 @@ export default function Locations() {
 
   return (
     <>
-      <Header />
-      <div className="flex justify-around h-full">
-        <div className="flex-1">
-          {Locations.map((location, index) => (
-            <div key={index}>
-              <a
-                href={`/locations/${location.id}`}
-                className="text-gray-600 hover:text-black"
-              >
-                {location.name}
-              </a>
+      {Locations.length === 0 ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+
+          <div className="flex justify-around h-full">
+            <div className="flex-1 p-6">
+              <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                {Locations.slice(0, 6).map((campground, index) => (
+                  <Link
+                    key={index}
+                    href={`/locations/${campground.id}`}
+                    className="text-gray-600 hover:text-black"
+                  >
+                    <CampgroundCard campground={campground} />
+                  </Link>
+                ))}
+              </div>
             </div>
-          ))}
-        </div>
-        <div className="flex-2 pl-2 border-l-2 border-b-2 max-h-full">
-          {" KARTA "}
-        </div>
-      </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
