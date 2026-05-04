4dc79e006b1886518b970ae1584c1e3c58e1c365
goran131 <goran.rosenberg@tomelilla.nu>
2026-05-04 19:18:54 +0200

    Tagit bort all button css:en från globals.css och ersatt med tailwind klasser i komponenten

--- app/buttons/page.js
+++ app/buttons/page.js
@@ -1,12 +1,10 @@
 "use client";
 
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Button from "@/components/Button";
-import "./styles.css";
+import FilterBar from "@/components/FilterBar";
 
 export default function Buttons() {
-  
+
     function alertPrimary() {
       alert('Primary button clicked!');
     }
@@ -14,24 +12,22 @@ export default function Buttons() {
   return (
     <>
       <link rel="stylesheet" href="https://googleapis.com" />
-      <Header />
       <div className="m-10">
         <h1>Buttons</h1>
-        
-        <Button variant="primary" extraCssClasses="mr-3" onClick={() => { alertPrimary() }}>Login</Button>                  
+
+        <Button variant="primary" extraCssClasses="mr-3" onClick={() => { alertPrimary() }}>Login</Button>
 
         <Button variant="secondary" extraCssClasses="mr-3" onClick={() => { alert("Secondary button clicked") }}>Login</Button>
-     
+
         <Button variant="outlined" extraCssClasses="mr-3">Outlined</Button>
 
-        <Button variant="search" extraCssClasses="testclass mr-3">Search</Button>
-     
+        <Button variant="search" extraCssClasses="mr-3">Search</Button>
+
         <Button variant="arrow-backward" extraCssClasses="mr-3"/>
 
         <Button variant="arrow-forward"  extraCssClasses="mr-3"/>
-        
       </div>
-      <Footer />
+
     </>
   );
 }
--- components/Button.js
+++ components/Button.js
@@ -1,48 +1,50 @@
 "use client";
-/** Button komponenten har fem varianter, "primary", "secondary", "search", "arrow-backward", och "arrow-forward". 
-   Variant anges via "variant" prop. Man kan ange en "buttonText" prop för att sätta texten på knappen, 
-   och en "onclick" prop för att ange en funktion som ska köras när knappen klickas. Det går även att skicka 
-   in egna css klasser via "extraCssClasses" prop.
+/** Button komponenten har sex varianter, "primary", "secondary", outlined, "search", "arrow-backward", och "arrow-forward".
+   Variant anges via "variant" prop. Sätta texten på knappen görs genom att skicka texten den som children. Knappen har även en "onClick" prop
+   för att ange en funktion som ska köras när knappen klickas. Det går även att skicka sin egna css klasser via "extraCssClasses" prop.
 */
-export default function Button({ children, variant = "primary", onClick = null, extraCssClasses = ""}) {
-    if (variant === "primary") {
+export default function Button({ children, variant = "primary", onClick = null, extraCssClasses = ""})
+{
+   const buttonCss = "transition-transform duration-200 hover:-translate-y-0.5 transition-all active:scale-95 hover:shadow-[0_4px_12px_rgba(0,51,102,0.3)] cursor-pointer";
+
+   if (variant === "primary") {
         return (
-            <button className={`cursor-pointer not-first:text-white transition-all active:scale-95 pl-5 pr-5 pt-2 pb-2 rounded-3xl bg-green-700 ${extraCssClasses}`} onClick={ onClick != null ? onClick : null } >
+            <button className={`${extraCssClasses} ${buttonCss} text-white pl-5 pr-5 pt-2 pb-2 rounded-3xl bg-green-700`} onClick={ onClick != null ? onClick : null } >
                 { children }
             </button>
         );
     }
     else if (variant === "secondary") {
         return (
-            <button className={`cursor-pointer text-black transition-all active:scale-95 pl-5 pr-5 pt-2 pb-2 rounded-3xl secondary bg-green-300 ${extraCssClasses}`} onClick={ onClick != null ? onClick : null }>
+            <button className={`${extraCssClasses} ${buttonCss} text-black pl-5 pr-5 pt-2 pb-2 rounded-3xl secondary bg-green-300`} onClick={ onClick != null ? onClick : null }>
                 { children }
             </button>
         );
     }
     else if (variant === "outlined") {
         return (
-            <button className={`cursor-pointer text-black transition-all active:scale-95 pl-5 pr-5 pt-2 pb-2 rounded-3xl border secondary bg-white ${extraCssClasses}`} onClick={ onClick != null ? onClick : null }>
+            <button className={`${extraCssClasses} ${buttonCss} text-black pl-5 pr-5 pt-2 pb-2 rounded-3xl border secondary bg-white`} onClick={ onClick != null ? onClick : null }>
                 { children }
             </button>
         );
     }
     else if (variant === "search") {
         return (
-            <button className={`cursor-pointer text-white transition-all active:scale-95 pl-5 pr-5 pt-2 pb-2 rounded-xl bg-green-700 ${extraCssClasses}`} onClick={ onClick != null ? onClick : null }>
+            <button className={`${buttonCss} text-white pl-5 pr-5 pt-2 pb-2 rounded-xl bg-green-700 ${extraCssClasses}`} onClick={ onClick != null ? onClick : null }>
                 <img src="../images/icons/search-icon.webp" className="w-5 h-5 mr-1 inline" />{ children }
             </button>
-        );        
+        );
     }
     else if (variant === "arrow-forward") {
         return (
-            <button className={`cursor-pointer text-white text-xl transition-all active:scale-95 pl-4 pr-4 pt-1 pb-1 rounded-lg bg-green-700 ${extraCssClasses}`} onClick={ onClick != null ? onClick : null }> 
+            <button className={`${extraCssClasses} ${buttonCss} text-white text-xl pl-4 pr-4 pt-1 pb-1 rounded-lg bg-green-700`} onClick={ onClick != null ? onClick : null }>
                 <b>&gt;</b>
             </button>
         );
     }
     else if (variant === "arrow-backward") {
         return (
-            <button className={`cursor-pointer text-white text-xl transition-all active:scale-95 pl-4 pr-4 pt-1 pb-1 rounded-lg bg-green-700 ${extraCssClasses}`} onClick={ onClick != null ? onClick : null }> 
+            <button className={`${extraCssClasses} ${buttonCss} text-white text-xl pl-4 pr-4 pt-1 pb-1 rounded-lg bg-green-700`} onClick={ onClick != null ? onClick : null }>
                 <b>&lt;</b>
             </button>
         );
