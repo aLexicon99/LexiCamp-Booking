cfb6b8c03d71688bffd2100b1ef315d30f839150
halime <halimeucarturk@gmail.com>
2026-05-06 11:33:28 +0200

    add login page

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -6,17 +6,18 @@ import {useCallback} from "react";
 
 export default function useAuth() {
     const [user, setUser, logout] = useLocalStorage("auth_user", null);
-    const login = useCallback((username) => {
+    const login = useCallback((username, password) => {
         const userToLogin = username.trim().toLocaleLowerCase();
-        const userData = allUsers.find((_user) => _user.name === userToLogin);
-        if (userData) {
+        const userData = allUsers.find((_user) => _user.username === userToLogin || _user.email === userToLogin);
+        if (userData && userData.password === password) {
             setUser(userData);
 
             return true;
         }
 
         return false;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    return [user, login, logout]
+    return [user, login, logout];
 }
