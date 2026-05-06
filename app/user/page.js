8a5dbddba930ec894c1459f96a6ec3ba35bb30e0
goran250 <goran.ros@gmail.com>
2026-05-06 21:59:49 +0200

    Designat user sidan och lagt in två hårdkodade bokningar

--- app/user/page.js
+++ app/user/page.js
@@ -4,8 +4,10 @@ import {useEffect} from "react";
 import useAuth from "@/hooks/useAuth";
 import {redirect} from "next/navigation";
 import useIsClient from "@/hooks/useIsClient";
+import Button from "@/components/Button"
 
-export default function Page() {
+
+export default function page() {
   const [user, _, logout] = useAuth();
   const isClient = useIsClient();
 
@@ -20,31 +22,28 @@ export default function Page() {
   }
 
   return (
-    <div>
-        <>
-          {/*✅ INLOGGAD */}
-          <button
-            className="w-full bg-orange-600 text-white p-2"
-            onClick={logout}
-          >
+    <div className="max-w-md m-auto">
+        {/*✅ INLOGGAD */}
+        <Button variant="primary" onClick={logout} extraCssClasses="ml-auto mr-auto mt-5 mb-5 block">
             Logga ut - {user.firstName}
-          </button>
-          <pre className="bg-gray-300">{JSON.stringify(user, null, 4)}</pre>
-          {/* {
-            <div className="mt-2">
-              <p> Tidigare bokningar : {userData.bookings.length}</p>
-              {userData.bookings && userData.bookings.length > 0 && (
-                <>
-                  {userData.bookings.map((booking, index) => (
-                    <p key={index}>
-                      <a href={`/locations/${booking.id}`}>{booking.id}</a>
-                    </p>
-                  ))}
-                </>
-              )}
-            </div>
-          } */}
-        </>
+        </Button>
+        <div className="ml-[15] mr-[15] bg-gray-300 pt-6 px-[30] py-[30] rounded-[12] shadow-[5px_4px_20px_rgb(170,170,170)]">
+            <h2 className="text-xl text-green-700"><b>User data</b></h2>
+            <div className="my-[15]"><b>Name: </b> {user.firstName} {user.lastName }</div>
+            <div className="my-[15]"><b>Username: </b> {user.username }</div>
+            <div className="my-[15]"><b>Email: </b> {user.email }</div>
+            <h2 className="mt-[25] text-xl text-green-700"><b>Previous bookings</b></h2>
+            <hr className="w-full mt-[15] mb-[15]"/>
+            <p><b>Camping site: </b>Bingsmarkens Camping</p>
+            <p><b>Start date: </b>2026-04-15</p>
+            <p><b>End date: </b>2026-04-18</p>
+            <p><b>Total price: </b>3560 kr</p>
+            <hr className="w-full mt-[15] mb-[15]"/>
+            <p><b>Camping site: </b>Borrbystrand Camping</p>
+            <p><b>Start date: </b>2025-07-10</p>
+            <p><b>End date: </b>2025-07-16</p>
+            <p><b>Total price: </b>4560 kr</p>
+        </div>
     </div>
   );
 }
