5235a908f49d67fa7a4ddfce8fa1207c0357821c
aLexicon99 <267594691+aLexicon99@users.noreply.github.com>
2026-05-07 12:10:46 +0200

    Filter-Sort-Bar
    new Filter / Sort Bar
    -  i only used sort by Price and Rating for now

---
+++ app/campsites/_components/FilterBar.js
@@ -0,0 +1,74 @@
+"use client";
+
+import { redirect } from "next/navigation";
+
+export default function FilterBar({
+  priceOnClick,
+  ratingOnClick,
+  campsites,
+  search,
+  guests,
+  date,
+}) {
+  const hasURLparams = window.location.search != "";
+  return (
+    <section className="mb-10">
+      <div className="flex flex-col md:flex-row md:items-center justify-between gap-6 bg-surface-container-lowest p-6 rounded-2xl shadow-[0_8px_30px_rgba(27,67,50,0.04)] border border-outline-variant/30">
+        <div className="flex-1">
+          <h1 className="text-2xl font-extrabold text-primary mb-2">
+            Campgrounds
+          </h1>
+          <p className="text-on-surface-variant">
+            {campsites.length} results found{" "}
+            {search && campsites.length > 0 ? `for "${search}"` : ""}
+          </p>
+          <p className="font-semibold text-green-900">
+            {date && `🗓️ ${date}`}{" "}
+            {guests && guests != "group"
+              ? `for "${guests} Guests + Beds"`
+              : guests === "group" && `for a ${guests.toUpperCase()}`}
+          </p>
+        </div>
+
+        <div className="flex flex-wrap items-center gap-3">
+          {/* Pris */}
+          <button
+            onClick={() => priceOnClick()}
+            className="flex cursor-pointer items-center gap-2 px-4 py-2.5 rounded-xl border border-outline-variant hover:border-primary transition-all text-sm font-semibold"
+          >
+            <span className="material-symbols-outlined text-[20px]">
+              payments
+            </span>
+            Price
+          </button>
+
+          {/* Rating */}
+          <button
+            onClick={() => ratingOnClick()}
+            className="flex cursor-pointer items-center gap-2 px-4 py-2.5 rounded-xl border border-outline-variant hover:border-primary transition-all text-sm font-semibold"
+          >
+            <span className="material-symbols-outlined text-[20px]">
+              star_rate
+            </span>
+            Rating
+          </button>
+
+          {hasURLparams && (
+            <>
+              <div className="h-8 w-px bg-outline-variant mx-2"></div>
+              <button
+                onClick={() => redirect("/campsites")}
+                className="flex cursor-pointer items-center gap-2 px-4 py-2.5 bg-primary text-white rounded-xl hover:bg-opacity-90 transition-all text-sm font-bold shadow-lg shadow-primary/20"
+              >
+                <span className="material-symbols-outlined text-[20px]">
+                  close
+                </span>
+                Reset
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/campsites/page.js
+++ app/campsites/page.js
@@ -4,10 +4,67 @@ import Link from "next/link";
 export const dynamic = "force-dynamic";
 import Loading from "@/components/Loading";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import CampgroundCard from "@/components/CampgroundCard";
+import FilterBar from "./_components/FilterBar";
 
 export default function Locations() {
   const [campsites, setCampsites] = useState([]);
+  const [sortByPrice, setSortByPrice] = useState(false);
+  const [sortByRating, setSortByRating] = useState(false);
+
+  // URL SEARCH-PARAMS
+  const searchParams = useSearchParams();
+  const urlLocation = searchParams.get("location");
+  const urlGuests = searchParams.get("guests");
+  const urlDate = searchParams.get("checkIn");
+
+  function filterBySearchName(list, searchTerm) {
+    const filterResult = list.filter((campsite) => {
+      const name = campsite.name || "";
+      const city = campsite.city || "";
+
+      return (
+        name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        city.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
+
+    console.log("AFTER filterResult", filterResult);
+    return filterResult;
+  }
+
+  function SortByPriceClick() {
+    let sortedCampingsites = [];
+    if (sortByPrice) {
+      // HighToLow
+      sortedCampingsites = [...campsites].sort(
+        (a, b) => b.pricePerNight - a.pricePerNight,
+      );
+    } else {
+      // LowToHigh
+      sortedCampingsites = [...campsites].sort(
+        (a, b) => a.pricePerNight - b.pricePerNight,
+      );
+    }
+
+    setCampsites(sortedCampingsites);
+    setSortByPrice(!sortByPrice);
+  }
+
+  function SortbyRatingClick() {
+    let sortedCampingsites = [];
+    if (sortByRating) {
+      // HighToLow
+      sortedCampingsites = [...campsites].sort((a, b) => b.rating - a.rating);
+    } else {
+      // LowToHigh
+      sortedCampingsites = [...campsites].sort((a, b) => a.rating - b.rating);
+    }
+
+    setCampsites(sortedCampingsites);
+    setSortByRating(!sortByRating);
+  }
 
   useEffect(() => {
     async function fetchLocations() {
@@ -26,6 +83,14 @@ export default function Locations() {
         <>
           <div className="flex justify-around h-full">
             <div className="flex-1 p-6">
+              <FilterBar
+                ratingOnClick={SortbyRatingClick}
+                priceOnClick={SortByPriceClick}
+                campsites={campsites}
+                search={urlLocation}
+                guests={urlGuests}
+                date={urlDate}
+              />
               <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
                 {campsites.map((campground) => (
                   <Link
